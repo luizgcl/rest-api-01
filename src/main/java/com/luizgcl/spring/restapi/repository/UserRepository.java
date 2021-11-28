b611a25a749c698d1c7e1ee9c6f719e444963636
Luiz Gustavo <luiizgcl@gmail.com>
2021-11-28 00:49:16 -0300

    Updated method "findById" in user service.

--- src/main/java/com/luizgcl/spring/restapi/repository/UserRepository.java
+++ src/main/java/com/luizgcl/spring/restapi/repository/UserRepository.java
@@ -3,9 +3,7 @@ package com.luizgcl.spring.restapi.repository;
 import com.luizgcl.spring.restapi.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends MongoRepository<User, String> {
-
-    User findById(Long id);
+public interface UserRepository extends MongoRepository<User, Long> {
 
     User findByName(String name);
 
--- src/main/java/com/luizgcl/spring/restapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/luizgcl/spring/restapi/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -21,7 +22,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(Long id) {
-        return this.userRepository.findById(id);
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElse(null);
     }
 
     @Override
